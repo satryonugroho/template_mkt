9f0004497fc1bd504119b5869f177444545aae98
satryonugroho <n.p.satryo@gmail.com>
2023-12-28 10:27:24 +0700

    Update Readme

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # React + Vite + Tailwind + Heroicons
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This template use vite/react + tailwind + react-router + Heroicons + Atomic Desain
 
-Currently, two official plugins are available:
+use this template -> Create a new repository
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+npm install
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import reactLogo from '../../assets/imgs/react.svg'
 import viteLogo from '../../assets/imgs/vite.svg'
 import tailwindLogo from '../../assets/imgs/tailwind.svg'
+import axios from 'axios'
 
 const Home = () => {
+  useEffect(()=>{
+    let data = JSON.stringify({
+      "username": "SatryoNugroho",
+      "password": "Zenita19"
+    });
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'http://127.0.0.1:8080/api/user',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+
+    axios.request(config)
+    .then((response) => {
+      console.log("Response: ",response);
+    })
+    .catch((error) => {
+      console.error("Error: ",error);
+    });
+  },[])
   return (
     <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='flex'>
